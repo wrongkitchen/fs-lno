ee0365f11b63926d2e50be51029d88e7f820096b
wrongkitchen <wkc.kenji@gmail.com>
2013-06-01 16:04:08 +0800

    first commit

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+	<div class="container">
+		<div class="pull-left"><img src="img/footer-slogan.png" alt=""></div>
+		<div class="pull-right">
+			<span class="tnc">Terms and Conditions</span>
+			<span class="copyright">Copyright © 2013 L&O Limited. All Rights Reserved.</span>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<script type="text/javascript" src="js/ga.js"></script>
---
+++ include/header.php
@@ -0,0 +1,33 @@
+<div id="header">
+	<div class="container">
+		<a href="javascript:void(0)" onclick="window.sectionManager.changeSection('landingSection')"><div class="logo"></div></a>
+		<div class="lanuage">
+			<span class="langBtn">ENG</span>
+			<span class="langBtn">繁中</span>
+			<span class="langBtn">簡中</span>
+			<span class="langBtn">المؤس</span>
+		</div>
+
+		<div class="navigator">
+			<a href="#aboutSection">
+				<div class="navButton">ABOUT US</div>
+			</a>
+			<div class="navSlash">/</div>   
+			<a href="#projectSection"><div class="navButton">PROJECTS</div></a>
+			<div class="navSlash">/</div> 
+			<a href="#newsSection"><div class="navButton">NEWS & MEDIA</div></a>
+			<div class="navSlash">/</div> 
+			<a href="#awardSection"><div class="navButton">AWARDS</div></a>
+			<div class="navSlash">/</div> 
+			<a href="#susSection"><div class="navButton">SUSTAINABILITY</div></a>
+			<div class="navSlash">/</div>
+			<a href="#careerSection"><div class="navButton">CAREER</div></a>
+			<div class="navSlash">/</div> 
+			<a href="#contactSection"><div class="navButton">CONTACTS</div></a>
+			<div class="navSlash end">/</div> 
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="lineBlock"></div>
+	</div>
+</div>
